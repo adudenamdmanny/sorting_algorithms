d59fd2734164629db6fa68205c3ad4cc08d517e2
Emmanuel Essien <117782578+adudenamdmanny@users.noreply.github.com>
2023-04-23 05:00:39 +0100

    Update sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,10 @@
-#ifndef _SORT_
-#define _SORT_
+#ifndef SORT_H
+#define SORT_H
 
+
+#include <stddef.h>
 #include <stdlib.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -17,19 +20,28 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/* Print Functions */
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+/* Sorting Algorithms Functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void _qsort(int *a, int low, int high, int size);
+
+
+/*Advanced Tasks*/
 void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
-#endif
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
